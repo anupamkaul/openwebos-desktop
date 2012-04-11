a2c6337df15925784b32939e815b2f3bde607aa4
Anupam Kaul <anupam.kaul@yahoo.com>
2012-04-10 17:59:43 -0700

    Add build files for cjson and luna-service2. Set CJSON variables in luna-service2 to point to the include and lib folders from the correct staging folder.

---
+++ scripts/build_luna-service2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. ./common/envsetup.sh
+
+NAME=$1
+PROCCOUNT=$2
+
+cd $WEBOS_DESKTOP_ROOT/$NAME/src
+cmake . -DCMAKE_INSTALL_PREFIX=$LUNA_STAGING -DCMAKE_BUILD_TYPE=RELEASE
+make -j$PROCCOUNT
+make install
+
+cd $STARTDIR
+
+
