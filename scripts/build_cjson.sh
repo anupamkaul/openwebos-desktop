43954a2b450c5f6c930704b867ad3f6075d228e1
Anupam Kaul <anupam.kaul@yahoo.com>
2012-04-13 12:41:07 -0700

    change build scripts for cjson and pmloglib

--- scripts/build_cjson.sh
+++ scripts/build_cjson.sh
@@ -2,6 +2,15 @@
 
 . ./common/envsetup.sh
 
-. ./common/makesetup.sh
+NAME=$1
+PROCCOUNT=$2
 
-build_target $WEBOS_DESKTOP_ROOT $1 $2
+cd $WEBOS_DESKTOP_ROOT/$NAME
+sh autogen.sh
+./configure --disable-static --prefix=$LUNA_STAGING
+make -j$PROCCOUNT
+make install
+
+echo "STARTDIR IS $STARTDIR"
+cd $STARTDIR
+
